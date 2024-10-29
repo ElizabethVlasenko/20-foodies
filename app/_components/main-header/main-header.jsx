2e3d9950a4c8f4bf899920889e4ed958891af5fa
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-10-29 14:10:54 +0000

    Add content to the community page and highlight the nav link for the current page

--- app/_components/main-header/main-header.jsx
+++ app/_components/main-header/main-header.jsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -18,10 +20,11 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
+
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community"> Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
