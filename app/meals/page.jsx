ae3f5f3fdaab80ba8292eefca03cb555707841fe
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-10-29 14:33:16 +0000

    Add a content for the meals page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import MealsGrid from "../_components/meals/meals-grid";
+
+import classes from "./page.module.css";
+
 function Page() {
-  return <h1>Meals page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipes and cook it yourself </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
 export default Page;
