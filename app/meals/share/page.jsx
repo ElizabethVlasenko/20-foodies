7f5a52a00ed54a4679000aa278f7d715369bc2f2
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-10-30 16:45:32 +0000

    Add form validation and error responses

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,10 +1,14 @@
+"use client";
+
 import ImagePicker from "@/app/_components/meals/image-picker";
 import classes from "./page.module.css";
+import { useFormState } from "react-dom";
 
 import { shareMeal } from "@/app/_lib/actions";
 import MealsFormSubmit from "@/app/_components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +47,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker name="image" label="Your image" />
+          {state.message && <p className={classes.error}>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
